68b0031bcbad2718ee27880c9878b861f21b3d8a
peytonkemp <peyton.kemp@gmail.com>
2021-02-16 11:29:38 -0500

    added delete button

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -1,3 +1,7 @@
+import { deleteNote } from "./NoteDataProvider.js"
+
+const eventHub = document.querySelector(".container")
+
 export const NoteHTMLConverter = (noteObject, criminalObject) => {
     return `
         <section class="note">
@@ -6,6 +10,14 @@ export const NoteHTMLConverter = (noteObject, criminalObject) => {
             <div class="note__author">Author: ${ noteObject.author }</div>
             <div class="note__date">Date: ${ new Date(noteObject.date).toLocaleDateString('en-US')  }</div>
             <div class="note__intuition">Intuition: ${ noteObject.intuition }</div>
+            <button id="deleteNote--${noteObject.id}">Delete</button>
         </section>
     `
 }
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id.startsWith("deleteNote--")) {
+        const [prefix, id] = clickEvent.target.id.split("--")
+        deleteNote(id)
+    }
+})
